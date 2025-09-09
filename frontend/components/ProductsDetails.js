1221f520323fd906a2174a2e8fca810e2054e942
Amith <amithajith7025@gmail.com>
2025-09-09 15:14:37 +0530

    created product route skeleton

--- frontend/components/ProductsDetails.js
+++ frontend/components/ProductsDetails.js
@@ -1,5 +1,20 @@
 import React,{useState,useEffect} from "react";
 import Link from "next/link";
+import { Box, Typography, Container } from "@mui/material";
+import { styled } from '@mui/system';
+
+const MainContainer = styled(Box)(({theme}) => ({
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+    padding: '20px'
+}))
+
+const ChildBox = styled(Box)(({theme}) => ({
+    width: '300px',
+    height: '200px'
+}))
 
 
 function ProductsDetails(){
@@ -33,26 +48,26 @@ function ProductsDetails(){
     
 
     return(
-        <div className="product-grid">
+        <MainContainer>
 
                 {
                     products.map((obj) => (
-                        <div className="product" key={obj.id}>
+                        <ChildBox key={obj.id}>
 
-                            <img src={obj.image} alt="alternative"></img>
+                            <img src='/mnt/stone/projects/freelance/sports-project/image.png' alt="alternative"></img>
                             <h2>{obj.title}</h2>
                             <p>
                                 <span>{obj.price}</span>
                             </p>
-                            <Link href={`/product/${obj.id}`}>
+                            <Link href={`/products/1/`}>
                             <button>product details</button>
                             </Link>
 
-                        </div>
+                        </ChildBox>
                     ))
                 }
 
-        </div>
+        </MainContainer>
     )
 
 }

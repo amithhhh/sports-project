fb73f1aba0067487bab4c57493a64edc391a72cb
Amith <amithajith7025@gmail.com>
2025-09-18 20:35:06 +0530

    create product routes

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     path('api/auth/', include('dj_rest_auth.urls')),
     path('api/auth/register/', include('dj_rest_auth.registration.urls')),
     path('api/user/', include('users.urls')),
-    path('api/predict/', include('predict.urls'))
+    path('api/predict/', include('predict.urls')),
+    path('api/products/', include('products.urls'))
 ]
 
 if settings.DEBUG:
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import productSerializer
+from .models import Product
 
 # Create your views here.
+
+class ProductListCreateView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = productSerializer
+
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = productSerializer

b917742122337517ca31000ff67083e6a6bda5fd
Amith <amithajith7025@gmail.com>
2025-10-17 05:01:33 +0530

    created additional fields in user table

--- backend/users/models.py
+++ backend/users/models.py
@@ -7,12 +7,14 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)  
     username = models.CharField(max_length=150, unique=True)
-
-
-
-class AdditionalInfo(models.Model):
-    pass
+    mobile_number = models.CharField(max_length=15, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    city = models.CharField(max_length=100, blank=True, null=True)
+    state = models.CharField(max_length=100, blank=True, null=True)
+    postal_code = models.CharField(max_length=10, blank=True, null=True)
     
+    def __str__(self):
+        return self.username
 
 
 
